b6c92fdb73b0f005c3292f439c577183ba206e6f
kei <kei.takakuda.0415@icloud.com>
2019-07-15 22:05:27 +0900

    add Flag

--- main.go
+++ main.go
@@ -13,8 +13,20 @@ func main() {
 	app.Version = "0.0.1"
 
 	app.Action = func(context *cli.Context) error {
-		fmt.Println(context.Args().Get(0))
+		if context.Bool("test") {
+			fmt.Println(context.Args().Get(0) + "test")
+		} else {
+			fmt.Println(context.Args().Get(0))
+		}
+
 		return nil
+	}
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "test, t",
+			Usage: "Echo with test",
+		},
 	}
 
 	app.Run(os.Args)
